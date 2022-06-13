508d4a2938c7bf57a415b6530f88f928c2b3908f
ry10an23 <kawaguchi.r823@gmail.com>
2022-06-13 11:16:40 -0700

    add weather api

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -2,9 +2,12 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=7be840106bd6433d60763025e19c7a87`
+  className
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="app">
+
     </div>
   );
 }
